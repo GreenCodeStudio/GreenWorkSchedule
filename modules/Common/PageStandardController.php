4ca80eb9e9e445d55720c7a1116cbb94ac9dd61d
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2026-09-17 22:48:33 +0200

    .

--- modules/Common/PageStandardController.php
+++ modules/Common/PageStandardController.php
@@ -3,5 +3,11 @@
 namespace Common;
 
 class PageStandardController extends \Core\StandardController
-{}
+{
+    use \CommonBase\PageStandardControllerTrait;
+    public function getPageTitle(): string
+    {
+        return "GreenWorkSchedule";
+    }
+}
 
